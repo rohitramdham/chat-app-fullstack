96ec52c0bd587a7e38465515b6db49fdfc6a4260
rohitramdham08 <133022780+rohitramdham08@users.noreply.github.com>
2025-11-15 01:19:34 +0530

    Fixed production route for Render deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,9 +25,9 @@ const __dirname = path.resolve();
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "development") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
